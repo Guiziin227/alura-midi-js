1d238ea9c977120fad7d5b44814ababb05b7b375
Guiziin227 <gh63388@gmail.com>
2023-03-08 15:24:59 -0300

    Projeto página dinâmica com alura

---
+++ index.js
@@ -0,0 +1,32 @@
+function tocaSom(seletorAudio){
+   const elemento = document.querySelector(seletorAudio);
+   
+   if(elemento != null && elemento.localName === 'audio'){
+     elemento.play()
+   } else {
+      console.log('Error selecione audio')
+   }
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+ for(let contador = 0; contador < listaDeTeclas.length; contador += 1){
+   const tecla = listaDeTeclas[contador]
+   const instrumento = tecla.classList[1]
+   const idAudio = `#som_${instrumento}`;
+   tecla.onclick = function () {
+      tocaSom(idAudio)
+
+   tecla.onkeydown = function (event) {
+      if(event.code === 'Space' || event.code === 'Enter'){
+         tecla.classList.add('ativa')
+      }
+      
+   }
+   tecla.onkeyup = function () {
+      tecla.classList.remove('ativa')
+   }
+   }
+ }
+
+ 
